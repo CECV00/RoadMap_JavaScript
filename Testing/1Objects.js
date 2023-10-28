680c115228c109f07293e108906fd3d7b4439a74
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-10-28 10:01:17 -0500

    update in object Singleton with ESLint

--- Testing/1Objects.js
+++ Testing/1Objects.js
@@ -3,29 +3,29 @@
 /*
     Metodo 1 - construcutor de objetos
 */
-const mi_carro = new Object();
+const mi_carro = new Object()
 
 /*
     Metodo 2 - Literal de objetos o inicializador
 */
 
 const mi_moto = {
-  title: "kazaki",
-  motor: "150prd",
-  valor: 140000,
-};
+  title: 'kazaki',
+  motor: '150prd',
+  valor: 140000
+}
 
 /*
     Metodo 3 - Function constructora
 */
 
-function myBarco(name, año, precio) {
-  this.name1 = name;
-  this.año1 = año;
-  this.precio1 = precio;
+function myBarco (name, año, precio) {
+  this.name1 = name
+  this.año1 = año
+  this.precio1 = precio
 }
 
-const barco1 = new myBarco("remora1", 1990, 13000);
+const barco1 = new myBarco('remora1', 1990, 13000)
 // console.log(barco1);
 
 /*
@@ -33,20 +33,20 @@ const barco1 = new myBarco("remora1", 1990, 13000);
 */
 
 class MyAvion {
-  constructor(name, modelo, year, price) {
-    this.name1 = name;
-    this.modelo1 = modelo;
-    this.year1 = year;
-    this.price1 = price;
+  constructor (name, modelo, year, price) {
+    this.name1 = name
+    this.modelo1 = modelo
+    this.year1 = year
+    this.price1 = price
   }
 }
 
-const avion1 = new MyAvion("u993", "j27", 1996, 1200000);
+const avion1 = new MyAvion('u993', 'j27', 1996, 1200000)
 // console.log(avion1);
 
 /*
     ----------------------------------------------------------------------------------------------------
-    Metodo 5 - patron singleton 
+    Metodo 5 - patron singleton
     // el objeto se instancia una sola vez.
     // tomando los conceptos de los Closures y las IIFE (Funciones autoejecutables) podemos crear este patrón en el cuál el objeto se instancia una única vez.
 */
@@ -58,11 +58,11 @@ const avion1 = new MyAvion("u993", "j27", 1996, 1200000);
     }}();
 **/
 // no se recomeinda este metodo para crear objetos singletons
-const singl = new (function Singleton() {
-  this.name = "Carlos";
-})();
+const singl = new function Singleton () {
+  this.name = 'Carlos'
+}()
 
-console.log(singl);
+console.log(singl)
 
 /**
     1- Estás creando una instancia de un objeto anónimo y Singleton al mismo tiempo.
@@ -70,7 +70,7 @@ console.log(singl);
     2- Dentro de la función anónima, se define una propiedad name en this. La instancia de Singleton tendrá esta propiedad con el valor 'Carlos'.
     3- La instancia resultante se almacena en la variable singl.
     4- Finalmente, imprimes la variable singl en la consola.
-    
+
     El código funcionará, y singl contendrá una instancia del objeto Singleton con una
         propiedad name establecida en 'Carlos'. Sin embargo, es importante tener en cuenta que
         este no es un patrón de diseño Singleton convencional en JavaScript. El enfoque más
@@ -93,27 +93,27 @@ console.log(singl);
 **/
 
 const Singleton1 = (function () {
-  let instance;
+  let instance
 
-  function init() {
+  function init () {
     // Código para inicializar la instancia
     return {
-      name: "Carlos",
-    };
+      name: 'Carlos'
+    }
   }
 
   return {
     getInstance: function () {
       if (!instance) {
-        instance = init();
+        instance = init()
       }
-      return instance;
-    },
-  };
-})();
+      return instance
+    }
+  }
+})()
 
-const singleton1 = Singleton.getInstance();
-const singleton2 = Singleton.getInstance();
+const singleton1 = Singleton.getInstance()
+const singleton2 = Singleton.getInstance()
 
 // console.log(singleton1 === singleton2); // Esto debería imprimir true, ya que es la misma instancia
 
@@ -126,7 +126,7 @@ const singleton2 = Singleton.getInstance();
         la crea llamando a init() y luego la devuelve.
     5- Al final del IIFE, retornamos un objeto que contiene el método getInstance
         para obtener la instancia del Singleton.
-    
+
     Este patrón de diseño Singleton garantiza que solo haya una instancia de
         la clase Singleton y proporciona un punto de acceso global para acceder a ella.
 
@@ -135,36 +135,36 @@ const singleton2 = Singleton.getInstance();
 // INTENTO 3 - RECOMENDADA
 
 const Singleton = (function () {
-  let instance;
+  let instance
 
-  function createInstance() {
+  function createInstance () {
     // Cualquier lógica de inicialización que desees
     const obj = {
-      name: "Carlos",
-      age: 30,
-    };
-    return obj;
+      name: 'Carlos',
+      age: 30
+    }
+    return obj
   }
 
   return {
     getInstance: function () {
       if (!instance) {
-        instance = createInstance();
+        instance = createInstance()
       }
-      return instance;
-    },
-  };
-})();
+      return instance
+    }
+  }
+})()
 
 // Para obtener la instancia del Singleton:
-const singletonInstance1 = Singleton.getInstance();
-const singletonInstance2 = Singleton.getInstance();
+const singletonInstance1 = Singleton.getInstance()
+const singletonInstance2 = Singleton.getInstance()
 
 // Comprobamos si son la misma instancia
 // console.log(singletonInstance1 === singletonInstance2); // Esto debería imprimir true
 
 // Modificar la instancia
-singletonInstance1.name = "Juan";
+singletonInstance1.name = 'Juan'
 
 // Verificar que el cambio se refleja en la instancia compartida
 // console.log(singletonInstance2.name); // Esto imprimirá "Juan"
@@ -174,7 +174,7 @@ singletonInstance1.name = "Juan";
     2- La función createInstance se encarga de crear el objeto que deseamos que sea una instancia única.
     3- La función getInstance verifica si ya existe una instancia y, si no, la crea llamando a createInstance.
     4- Luego, podemos obtener la instancia del Singleton llamando Singleton.getInstance().
-    
+
     Ten en cuenta que, en este patrón Singleton, singletonInstance1 y singletonInstance2
     hacen referencia a la misma instancia del objeto, por lo que cualquier cambio en una de ellas
     se reflejará en la otra, como se muestra en el ejemplo al modificar el nombre en
